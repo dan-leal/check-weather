12418b5efd5fb87fe52d5c3836d364747f8a2599
root <root@Danzito-PC>
2023-10-12 22:05:27 -0400

    adding unit test type of erros and check

--- square.py
+++ square.py
@@ -1,5 +1,10 @@
 class Square:
     def __init__(self, length) -> None:
+        if type(length) not in [int, float]:
+            raise TypeError('Length must be an integer or float')
+        if length < 0:
+            raise ValueError('Length must not be negative')
+
         self.length = length
 
     def area(self):
--- test_square.py
+++ test_square.py
@@ -8,3 +8,12 @@ class TestSquare(unittest.TestCase):
         square = Square(10)
         area = square.area()
         self.assertEqual(area, 100)
+
+    def test_length_with_wrong_type(self):
+        with self.assertRaises(TypeError):
+            square = Square('10')
+
+    def test_length_with_zero_or_negative(self):
+        with self.assertRaises(ValueError):
+            square = Square(0)
+            square = Square(-1)
